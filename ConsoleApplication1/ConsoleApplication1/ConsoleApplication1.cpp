05114772948dc11b76c160f4bda3da421dd9fee0
kryla <kryla@gmail.com>
2016-10-31 04:52:10 +0300

    2 функции(работа нормальная)

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,13 +2,35 @@
 //
 
 #include "stdafx.h"
-#include "stdlib.h"
-#include "iostream"
+//#include "stdlib.h"
+#include "iostream"//работа ввода/вывода
+#include "cstdlib" //работа функции system
+#include <ctime> // содержит time()
 using namespace std;
+
+
+void random(int c2) //выделение памяти и наполнение массива
+{
+	srand(time(NULL));
+	int *m = (int*)malloc(c2 * sizeof(int)); //m матрица, с2 кол-во элементов 
+											//void* calloc(c, 1);
+
+	for (int i = 0; i < c2; i++)
+	{
+		m[i] = rand();
+		cout << m[i] << endl;
+	}
+}
 
 
 int main()
 {
-	int с = 0;
-	cout << "d";
+	setlocale(LC_ALL, "RUSSIAN");//поддержка русского языка
+	int c = 0;
+	cout << "Количество элементов матрицы = ";
+	cin >> c; 
+	cout << endl;
+	random(c);
+	
+	system("pause");
 }
