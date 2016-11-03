804053b8b2f9edd6e8f942782af7ea0f7bced5a5
kryla <kryla@gmail.com>
2016-11-03 11:28:04 +0300

    Оптимизация длины кода. Удалены ненужные фигурные скобочки

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -34,25 +34,15 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 	for (int i = 0; i < c; i++)
 		m[i] = (int*)malloc(c * sizeof(int));
 	for (int i = 0; i < c; i++)
-	{
 		for (int j = 0; j < c; j++)
-		{
 			m[i][j] = rand();
-		}
-	}
-
 	for (int i = 0; i < c; i++) //вывод 
 	{
 		for (int j = 0; j < c; j++)
-		{
 			cout << setw(5) << m[i][j] << " ";
-		}
 		cout << endl;
 	}
-
-
 	cout << endl;
-
 	for (int tc = 0; tc < t; tc++) 
 	{
 		if (c % 2 == 1)
@@ -61,9 +51,7 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 		for (int j = c - 1;j >= 0;j--) //карусель
 		{
 			if (j == 0 || j % 2 == 0)
-			{
-				for (int i = c - 1;i > 0; i--)
-				{
+				for (int i = c - 1;i > 0; i--)				
 					if (i == 1)
 					{
 						if (j == 0)
@@ -77,12 +65,7 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 							m[0][j] = m[0][j - 1];
 						}
 					}
-					else {
-						m[i][j] = m[i - 1][j];
-					}
-				}
-			}
-
+					else m[i][j] = m[i - 1][j];
 			if (j % 2 == 1)
 				for (int i = 1; i < c;i++)
 				{
@@ -94,7 +77,6 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 						m[i][j] = m[i + 1][j];
 				}
 		}
-
 		for (int i = 0; i < c; i++) //вывод 
 		{
 			for (int j = 0; j < c; j++)
