d06724cd6b895050c3278be3481d681378036411
kryla <kryla@gmail.com>
2016-11-03 11:04:03 +0300

    Карусель работает

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -49,7 +49,7 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 			m[i][j] = rand();
 		}
 	}
-
+	
 	for (int i = 0; i < c; i++) //вывод 
 	{
 		for (int j = 0; j < c; j++)
@@ -61,27 +61,54 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 		}
 		cout << endl;
 	}
+	
 
 	cout << endl;
-	
+	//m[0][0] = lstdgt;
 	for(int j=c-1;j>=0;j--) //карусель
 	{
-		if (j%2==0)
+		if (j==0||j%2==0)
 		{
-			for (int i = c;i > 0; i--)
-					if (i == 0)
-						m[0][j] = m[0][j-1];
+			for (int i = c - 1;i > 0; i--)
+			{
+				if (i == 1)
+				{
+					if (j == 0)
+					{
+						m[i][j] = m[i - 1][j];
+						m[0][0] = lstdgt;
+					}
 					else
-						if (j!=0)
-							m[i-1][j] = m[i-2][j];
-						else m[0][0] = lstdgt;
+					{
+						m[i][j] = m[i - 1][j];
+						m[0][j] = m[0][j - 1];
+					}
+				}
+				else {
+					//if (j != 0)
+						{
+							m[i][j] = m[i - 1][j];
+						}
+
+					/*else
+						{
+							m[i][j] = m[i - 1][j];
+							m[0][0] = lstdgt;
+						}*/
+					}
+			}
 		}
 
 		if (j % 2 == 1)
-			for (int i = 0; i <= c;i++)
-				if (i == c)
-					m[i - 1][j] = m[i - 1][j - 1];
-				else m[i - 1][j] = m[i - 2][j];
+			for (int i = 1; i < c;i++)
+			{
+				if (i == 1)
+					m[0][j] = m[1][j];
+				if (i == c - 1)
+					m[i][j] = m[i][j - 1];
+				else
+					m[i][j] = m[i + 1][j];
+			}
 		
 		/*if (j % 2 == 1)
 		{
@@ -146,7 +173,7 @@ void computing(int c) //выделение памяти, наполнение массива и его отображение
 	for (int i = 0; i < c; i++) //вывод 
 	{
 		for (int j = 0; j < c; j++)
-			cout << setw(5) << m[i][j] << " ";
+			cout << setw(10) << m[i][j] << " ";
 		cout << endl;
 	}
 
