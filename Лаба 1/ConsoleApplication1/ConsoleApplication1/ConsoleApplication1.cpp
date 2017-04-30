2387bc5111970103c4c6e9e098aea2489c342bd8
Илья Крылов <kryla79@gmail.com>
2017-04-30 14:47:39 +0500

    Добавлена первая лаба. Новая версия её утеряна

---
+++ Лаба 1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include <iostream>
+//#include <cmath>
+#include <locale>
+
+using namespace std;
+void display(unsigned int x);
+//глобальные переменные
+
+void display(unsigned int x) //перевод в BIN с разделением байтов
+{
+	for (int i = 0; i < sizeof(int)*4; ++i)
+	{
+		if (i && !(i % 8)) cout << " ";
+		cout << static_cast<bool>(x & 0x80000000);
+		x <<= 1;
+	}
+}
+
+void isSymmetry(unsigned int x) //проверка байтов на симметричность
+{
+	cout << endl;
+	for(int i=0; i<sizeof(int)/2;i++)
+	{
+		bool one, two, three, four;
+		one = (x&(1 << i * 8));
+		two = (x&(1 << 7 + i * 8));
+		three = (x&(1 << 1 + i * 8));
+		four = (x&(1 << 6 + i * 8));
+		if (((!one||two)&&(one||!two))&&((!three||four)&&(three||!four)))
+		{
+			
+		}
+	}
+}
+
+int main()
+{
+	unsigned int x;
+	cin >> x;
+	display(x);
+	system("PAUSE");
+}
