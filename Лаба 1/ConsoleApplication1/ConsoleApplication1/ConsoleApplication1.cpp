16b7106875d8e4b62bf028076e807123b2d15165
Илья Крылов <kryla79@gmail.com>
2017-04-30 15:04:36 +0500

    DEC->BIN function fixed

--- Лаба 1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Лаба 1/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,7 +9,7 @@ void display(unsigned int x);
 
 void display(unsigned int x) //перевод в BIN с разделением байтов
 {
-	for (int i = 0; i < sizeof(int)*4; ++i)
+	for (int i = 0; i < sizeof(int)*8; ++i)
 	{
 		if (i && !(i % 8)) cout << " ";
 		cout << static_cast<bool>(x & 0x80000000);
@@ -20,13 +20,13 @@ void display(unsigned int x) //перевод в BIN с разделением байтов
 void isSymmetry(unsigned int x) //проверка байтов на симметричность
 {
 	cout << endl;
-	for(int i=0; i<sizeof(int)/2;i++)
+	for(int i = 0; i < sizeof(int)/2; i++)
 	{
 		bool one, two, three, four;
-		one = (x&(1 << i * 8));
-		two = (x&(1 << 7 + i * 8));
-		three = (x&(1 << 1 + i * 8));
-		four = (x&(1 << 6 + i * 8));
+		one = (x & (1 << i * 8));
+		two = (x & (1 << 7 + i * 8));
+		three = (x & (1 << 1 + i * 8));
+		four = (x & (1 << 6 + i * 8));
 		if (((!one||two)&&(one||!two))&&((!three||four)&&(three||!four)))
 		{
 			
@@ -39,5 +39,6 @@ int main()
 	unsigned int x;
 	cin >> x;
 	display(x);
+	cout << endl;
 	system("PAUSE");
 }
